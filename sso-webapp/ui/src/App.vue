6ce430bf8cbffcee5cd566aad14b5108eb1e97fa
Jesse P. Johnson <jpj6652@gmail.com>
2020-02-05 23:23:35 -0500

    Implemented basic UI setup

---
+++ sso-webapp/ui/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Token />
+  </div>
+</template>
+
+<script>
+import Token from './components/Token.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Token
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ sso-webapp/ui/src/components/Token.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <h3>Token:</h3>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Insecure</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr> 
+          <th scope="row">{{insecure}}</th>
+        </tr>
+      </tbody>
+    </table> 
+  </div> 
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'Token',
+    data() {
+      return {
+        insecure: false,
+      };
+    },
+    created: function() {
+      axios
+        .get('http://localhost:3000/insecure')
+        .then(res => {
+          this.insecure = res.data;
+        })
+    }
+  }
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
